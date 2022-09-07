491193de5fc3500a7dd69629a30306f64c42a29d
Andrej Ceremnih <andrejceremnih@gmail.com>
2022-09-07 21:22:44 +0300

    changed one of the comments

--- exercises/exercise_2/main.go
+++ exercises/exercise_2/main.go
@@ -7,5 +7,5 @@ func change(x map[int]int) { x[1], x[2] = 11, 22 }
 func main() {
 	x := map[int]int{0: 1, 1: 2, 2: 3, 3: 4, 4: 5}
 	change(x)
-	fmt.Println(x) // Output: [0:1 1:22 2:33 3:4 4:5]   The map did change, so I can conclude that the maps in Go are references.
+	fmt.Println(x) // Output: [0:1 1:22 2:33 3:4 4:5]   The map did change, so I can conclude that maps in Go are references.
 }
